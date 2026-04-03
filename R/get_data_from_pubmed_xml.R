a59aa2cc284d197c637fb7f920d727073c6836ae
Stephen Royle <s.j.royle@warwick.ac.uk>
2026-04-03 11:25:13 +0100

    pkg basics, first function

---
+++ R/get_data_from_pubmed_xml.R
@@ -0,0 +1,197 @@
+#' Get data from PubMed XML
+#'
+#' This function reads a PubMed XML file and extracts relevant information into
+#' a data frame. It allows for optional inclusion of abstracts, publication
+#' dates, and other metadata such as MeSH headings, chemical names, grant
+#' information, and clinical trial identifiers.
+#'
+#' @param theFile The path to the PubMed XML file to be processed.
+#' @param include_all If TRUE, includes all optional fields (abstracts, dates, and other metadata).
+#' @param include_abstract If TRUE, includes the abstract text in the output data frame.
+#' @param include_dates If TRUE, includes publication dates (received, accepted, and pubmed) in the output data frame.
+#' @param include_other If TRUE, includes additional metadata such as MeSH headings, chemical names, grant information, and clinical trial identifiers in the output data frame.
+#'
+#' @returns A data frame containing the extracted information from the PubMed XML file, with columns for PMID, DOI, authors, year, article title, journal, volume, issue, pages, publication type, and optionally abstracts, dates, and other metadata.
+#' @export
+#'
+#' @examples
+
+get_data_from_pubmed_xml <- function(theFile,
+                                     include_all = FALSE,
+                                     include_abstract = FALSE,
+                                     include_dates = FALSE,
+                                     include_other = FALSE) {
+  if (include_all) {
+    include_abstract <- TRUE
+    include_dates <- TRUE
+    include_other <- TRUE
+  }
+  doc <- xml2::read_xml(theFile)
+  records <- xml2::xml_find_all(doc, "//PubmedArticle")
+
+  if (length(records) == 0) {
+    return(NULL)
+  }
+
+  collapse_nodes <- function(nodes, xpath, unique_values = FALSE) {
+    values <- vapply(nodes, function(node) {
+      matches <- xml2::xml_find_all(node, xpath)
+      text <- trimws(xml2::xml_text(matches))
+      text <- text[nzchar(text)]
+
+      if (unique_values) {
+        text <- unique(text)
+      }
+
+      if (length(text) == 0) {
+        return(NA_character_)
+      }
+
+      paste(text, collapse = "|")
+    }, character(1))
+
+    unname(values)
+  }
+
+  extract_authors <- function(nodes) {
+    values <- vapply(nodes, function(node) {
+      author_nodes <- xml2::xml_find_all(node, ".//Author")
+      author_nodes <- as.list(author_nodes)
+
+      safe_text <- function(author_node, xpath) {
+        value <- trimws(xml2::xml_text(xml2::xml_find_first(author_node, xpath)))
+        if (length(value) == 0 || is.na(value)) {
+          return("")
+        }
+        value
+      }
+
+      authors <- vapply(author_nodes, function(author_node) {
+        collective_name <- safe_text(author_node, "./CollectiveName")
+        if (nzchar(collective_name)) {
+          return(collective_name)
+        }
+
+        last_name <- safe_text(author_node, "./LastName")
+        initials <- safe_text(author_node, "./Initials")
+        fore_name <- safe_text(author_node, "./ForeName")
+
+        parts <- c(last_name, initials)
+        parts <- parts[nzchar(parts)]
+
+        if (length(parts) == 0 && nzchar(fore_name)) {
+          return(fore_name)
+        }
+
+        if (length(parts) == 0) {
+          return(NA_character_)
+        }
+
+        paste(parts, collapse = " ")
+      }, character(1))
+
+      authors <- authors[!is.na(authors) & nzchar(authors)]
+      if (length(authors) == 0) {
+        return(NA_character_)
+      }
+
+      paste(authors, collapse = "|")
+    }, character(1))
+
+    unname(values)
+  }
+
+  extract_year <- function(nodes) {
+    values <- vapply(nodes, function(node) {
+      year_value <- trimws(xml2::xml_text(xml2::xml_find_first(node, ".//PubDate/Year")))
+
+      if (!nzchar(year_value)) {
+        medline_value <- trimws(xml2::xml_text(xml2::xml_find_first(node, ".//PubDate/MedlineDate")))
+        year_value <- sub("^([0-9]{4}).*$", "\\1", medline_value)
+        if (!grepl("^[0-9]{4}$", year_value)) {
+          year_value <- NA_character_
+        }
+      }
+
+      year_value
+    }, character(1))
+
+    unname(values)
+  }
+
+  parse_pubmed_date <- function(nodes, status) {
+    month_lookup <- setNames(sprintf("%02d", seq_along(month.abb)), tolower(month.abb))
+
+    values <- vapply(nodes, function(node) {
+      date_node <- xml2::xml_find_first(node, paste0(".//PubMedPubDate[@PubStatus='", status, "']"))
+      if (inherits(date_node, "xml_missing")) {
+        return(NA_character_)
+      }
+
+      year_value <- trimws(xml2::xml_text(xml2::xml_find_first(date_node, "./Year")))
+      month_value <- trimws(xml2::xml_text(xml2::xml_find_first(date_node, "./Month")))
+      day_value <- trimws(xml2::xml_text(xml2::xml_find_first(date_node, "./Day")))
+
+      if (!nzchar(year_value) || !nzchar(month_value) || !nzchar(day_value)) {
+        return(NA_character_)
+      }
+
+      if (grepl("^[0-9]+$", month_value)) {
+        month_value <- sprintf("%02d", as.integer(month_value))
+      } else {
+        month_key <- tolower(substr(month_value, 1, 3))
+        month_value <- month_lookup[[month_key]]
+      }
+
+      if (is.null(month_value) || is.na(month_value)) {
+        return(NA_character_)
+      }
+
+      if (!grepl("^[0-9]+$", day_value)) {
+        return(NA_character_)
+      }
+
+      sprintf("%s-%s-%02d", year_value, month_value, as.integer(day_value))
+    }, character(1))
+
+    as.Date(unname(values))
+  }
+
+  theDF <- data.frame(
+    pmid = collapse_nodes(records, ".//MedlineCitation/PMID"),
+    doi = collapse_nodes(records, ".//ELocationID[@EIdType='doi']"),
+    authors = extract_authors(records),
+    year = extract_year(records),
+    articletitle = collapse_nodes(records, ".//ArticleTitle"),
+    journal = collapse_nodes(records, ".//ISOAbbreviation"),
+    volume = collapse_nodes(records, ".//JournalIssue/Volume"),
+    issue = collapse_nodes(records, ".//JournalIssue/Issue"),
+    pages = collapse_nodes(records, ".//MedlinePgn"),
+    ptype = collapse_nodes(records, ".//PublicationType"),
+    stringsAsFactors = FALSE
+  )
+
+  if (isTRUE(include_abstract)) {
+    theDF$abstract <- collapse_nodes(records, ".//Abstract/AbstractText")
+  }
+
+  if (isTRUE(include_dates)) {
+    theDF$recdate <- parse_pubmed_date(records, "received")
+    theDF$accdate <- parse_pubmed_date(records, "accepted")
+    theDF$pubdate <- parse_pubmed_date(records, "pubmed")
+  }
+
+  if (isTRUE(include_other)) {
+    theDF$meshHeadings <- collapse_nodes(records, ".//DescriptorName")
+    theDF$chemNames <- collapse_nodes(records, ".//NameOfSubstance")
+    theDF$grantAgency <- collapse_nodes(records, ".//Grant/Agency", unique_values = TRUE)
+    theDF$grantNumber <- collapse_nodes(records, ".//Grant/GrantID")
+    theDF$grantCountry <- collapse_nodes(records, ".//Grant/Country", unique_values = TRUE)
+    theDF$nctID <- collapse_nodes(
+      records,
+      ".//DataBank[DataBankName='ClinicalTrials.gov']/AccessionNumberList/AccessionNumber"
+    )
+  }
+
+  theDF
+}
--- README.md
+++ README.md
@@ -1,35 +1,5 @@
 # PubMedLagR
+
 Publication lag time analysis from PubMed data
 
-Get a summary of publication lag times from a PubMed XML file.
-
-1. Run a Pubmed search and download the file as XML. Example query: `nat commun[ta] AND 2000 : 2018[pdat] AND journal article[pt]`. Select _Send To: File: XML_.
-2. Run `pubmedlag.R` pointing R at your downloaded XML file.
-3. R will save a version of the data as csv, it will calculate publication lag time, make some plots and save as png.
-
-![](yearPlot.png)
-
-Three dates of interest are stored in a complete pubmed record.
-
-1. When the paper was _received_.
-2. When the paper was _accepted_.
-3. When the paper was _published_.
-
-Note that **these dates are not available for every record**. This is especially true of older papers.
-
-Because we have three dates, we can calculate three different time periods.
-
-1. From received to accepted (recacc).
-2. From received to published (recpub).
-3. From accepted to published (accpub).
-
-Most of the time the accpub time is short and constant for a journal, and so the mose interesting time is recacc.
-
-## More info
-
-The original `pubmedXML.R` is by [christopherBelter](https://github.com/christopherBelter/pubmedXML/commit/915ae0d873ef68360f6289dd90055c6f22dcaa18). It has been modified to retrieve the necessary data.
-
-For more on publication lag times:
-
-- Check out Daniel Himmelstein's [History of Delays](https://blog.dhimmel.com/history-of-delays/).
-- Posts at [quantixed on lag times](https://quantixed.org/tag/lag-times/).
+Under development, not yet ready for use.
